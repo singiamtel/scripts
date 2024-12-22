f385518523065b158d8813917520ff2be56ae9af
Sergio <sergio@garciadelacruz.es>
2024-12-23 00:04:13 +0100

    More warnings

--- narrow.py
+++ narrow.py
@@ -1,7 +1,9 @@
 import re
 
+
 def reg(pat, flags=0):
     return re.compile(pat, re.VERBOSE | flags)
+
 
 def matches(s, pat, *, case='smart', verbose=False):
     inverted = True
@@ -10,27 +12,30 @@ def matches(s, pat, *, case='smart', verbose=False):
         pat = pat[1:]
     flags = 0
     if case == 'off':
-        if verbose: print('case==of')
+        if verbose:
+            print('case==of')
         flags = re.IGNORECASE
     elif case == 'on':
-        if verbose: print('case==on')
-        pass
-    elif case == 'smart': # smart case
-        if verbose: print('case==smart')
+        if verbose:
+            print('case==on')
+    elif case == 'smart':  # smart case
+        if verbose:
+            print('case==smart')
         if not any(x.isupper() for x in pat):
-            if verbose: print('smartcase-ignore')
+            if verbose:
+                print('smartcase-ignore')
             flags = re.IGNORECASE
     else:
         raise ValueError('unknown case specification %s' % case)
     x = reg('(?:.*)' + pat, flags).match(s)
     return inverted == (not (not x))
 
-def narrow(strings, searches, **kws):
-  if isinstance(strings, str):
-    return len(narrow([strings], searches, **kws)) > 0
-  if isinstance(searches, str):
-    searches = [searches]
-  for search in searches:
-    strings = [s for s in strings if matches(s, search, **kws)]
-  return strings
 
+def narrow(strings, searches, **kws):
+    if isinstance(strings, str):
+        return len(narrow([strings], searches, **kws)) > 0
+    if isinstance(searches, str):
+        searches = [searches]
+    for search in searches:
+        strings = [s for s in strings if matches(s, search, **kws)]
+    return strings
